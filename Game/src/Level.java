8b3c4555ab95c7e19237a509d637bacbb1ce792b
Sean Ewing <seandabom08@gmail.com>
2013-04-04 11:34:50 -0700

    Level generation
    Larger update, still not completely functional. Missing Recursion, but
    will generate multidimensional array of level objects.

--- Game/src/Level.java
+++ Game/src/Level.java
@@ -1,13 +1,16 @@
 public class Level
 {
-	int numEnemies;
-	boolean enemy;
-	int numDoors = 2;
+	private int maxLevWidth = 7;
+	private int maxLevHeight = 7;
+	private int numEnemies;
+	private boolean enemy;
+	private int numDoors = 2;
 	private char nDoor = '#';
 	private char sDoor = '#';
 	private char eDoor = '#';
 	private char wDoor = '#';
 	private RandomGen rand;
+	protected Level[][] theLevel = new Level[maxLevHeight][maxLevWidth];
 	private boolean finalRoom = false;
 	private boolean firstRoom = false;
 	private boolean startDoor = false;
@@ -15,30 +18,55 @@ public class Level
 	protected Level south = null;
 	protected Level east = null;
 	protected Level west = null;
-	char i;
-	String level = null;
-	char door = '.';
-	char player = 'x';
-	char nplayer = '=';
-	char splayer = '=';
-	char eplayer = '=';
-	char wplayer = '=';
+	protected char i;
+	protected String level = null;
+	protected char door = '.';
+	protected char player = 'x';
+	protected char nplayer = '=';
+	protected char splayer = '=';
+	protected char eplayer = '=';
+	protected char wplayer = '=';
 	
+//  default
+//  [1]{[a][b][c][d][e][f][g]}
+//  [2]{[a][b][c][d][e][f][g]}
+//  [3]{[a][b][c][d][e][f][g]}
+//  [4]{[a][b][c][d][e][f][g]}
+//  [5]{[a][b][c][d][e][f][g]}
+//  [6]{[a][b][c][d][e][f][g]}
+//  [7]{[a][b][c][d][e][f][g]}
+//  
+//  [4][d] center room and starting room
+//  
+//  multidimensional arrays someArray[height][width]
+
 	public Level()
 	{
+		room();	
+	}
+	
+	public void room()//setter for the room.
+	{
+		for(int i=0; i<maxLevHeight; i++)
+		{
+			for(int j=0; j<maxLevWidth; j++)
+			{
+				theLevel[i][j] = null;
+			}
+		}
+		theLevel[maxLevWidth/2+1][maxLevHeight/2 +1] = createLevel(true);
+	}
+	
+	private Level createLevel(boolean first)//determines if this is the first room where the player enters into the level.
+	{
 		numEnemies = 0;
-		numDoors = 2;
 		try {rand = new RandomGen(1,4);}
 		catch (Exception e)
 		{
 			System.exit(100);
 			e.printStackTrace();
 		}
-//		if(rand.randomInt() % 2 == 0)
-//		{
-//			
-//		}
-		this.firstRoom = true;
+		
 		//32x16
 		sDoor ='/';
 		this.level =
@@ -56,49 +84,52 @@ public class Level
 			"#==============================#"+'\n'+
 			"#==============="+splayer+"==============#"+'\n'+
 			"###############"+sDoor+sDoor+"###############"+'\n';
-	}
-	//check min and max make sure separating is okay.
-	public Level genLevel(boolean enemies, int numEnemies, int minDoors, int maxDoors, char startDir) throws PreviousRoomException, Exception
-	{
-		rand = new RandomGen(minDoors, maxDoors);
-		this.numEnemies = numEnemies;
-		this.numDoors = minDoors + rand.randomInt();
-		if(numDoors == 1)
-			finalRoom = true;
 		return this;
-		
 	}
+	
+	private Level voidFillLevel(Level x)
+	{
+		//insert and fill level
+		return x;
+	}
+	
+//  check min and max make sure separating is okay.
+//  requires previous room direction?
+//	private Level genLevel(boolean enemies, int numEnemies, int minDoors, int maxDoors, char startDir) throws PreviousRoomException, Exception
+//	{
+//		rand = new RandomGen(minDoors, maxDoors);
+//		this.numEnemies = numEnemies;
+//		this.numDoors = minDoors + rand.randomInt();
+//		if(numDoors == 1)
+//			finalRoom = true;
+//		genLevel(true, numEnemies+rand.randomInt(),1,4);
+//		return this;
+//	}
 	
 	public String getLevel()
 	{
 		return level;
 	}
-	
-	/**
-	 * @param x
-	 * @param numN
-	 * @param numS
-	 * @param numE
-	 * @param numW
-	 */
-	public void recursiveLevel(Level x, int numN, int numS, int numE, int numW)
+
+	private void recursiveLevel(Level x, int numN, int numS, int numE, int numW)
 	{
-		if(x.north == null && numN >= 0)
-		{
-			x.north = new Level();
-			numN--;
-			while(x.north.finalRoom != true)
-			{
-				if(x.north.numDoors == 1)
-					x.north.finalRoom = true;
-			}
-			x = x.north;
-			recursiveLevel(x, numN, numS, numE, numW);
-		}
+//		if(x.north == null && numN >= 0)
+//		{
+//			x.north = new Level();
+//			numN--;
+//			while(x.north.finalRoom != true)
+//			{
+//				if(x.north.numDoors == 1)
+//					x.north.finalRoom = true;
+//			}
+//			x = x.north;
+//			recursiveLevel(x, numN, numS, numE, numW);
+//		}
 //		do recursion south
 //		do recursion east
 //		do recursion west
 	}
+
 	public void print()
 	{
 		System.out.println(level);
