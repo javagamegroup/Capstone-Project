a663e303e6c7046604a2c5c1538c7a6a76da52c9
Sean Ewing <seandabom08@gmail.com>
2013-05-16 12:53:55 -0700

    Added Level Manipulator

--- Game/src/game/Level.java
+++ Game/src/game/Level.java
@@ -344,38 +344,6 @@ public class Level
 				"############"+sDoor+sDoor+"############"+'\n';
 		return level;
 	}
-	
-	//possible set incomingDirection to lower case?
-	public void setPlayerEntrance(char incomingDirection) throws InvalidCharacterException
-	{
-		GamePanel.level.player = '=';
-		GamePanel.level.nplayer = '=';
-		GamePanel.level.splayer = '=';
-		GamePanel.level.eplayer = '=';
-		GamePanel.level.wplayer = '=';
-		
-		if(incomingDirection == 'n')
-		{
-			GamePanel.level = GamePanel.level.north;
-			GamePanel.level.splayer = 'x';
-		}
-		else if(incomingDirection == 's')
-		{
-			GamePanel.level = GamePanel.level.south;
-			GamePanel.level.nplayer = 'x';
-		}
-		else if(incomingDirection == 'e')
-		{
-			GamePanel.level = GamePanel.level.east;
-			GamePanel.level.wplayer = 'x';
-		}
-		else if(incomingDirection == 'w')
-		{
-			GamePanel.level = GamePanel.level.west;
-			GamePanel.level.eplayer = 'x';
-		}
-		else throw new InvalidCharacterException();
-	}
 
 	private void  recursiveLevel()
 	{
