d3594c92d7602523428adde3c2058924d4725533
Cody <shiltsc@sou.edu>
2013-05-30 14:30:43 -0700

    Working on Enemy rock collision

--- Game/src/game/EnemyAI.java
+++ Game/src/game/EnemyAI.java
@@ -114,15 +114,26 @@ public class EnemyAI extends Actor{
             }
         }
         
-        for (int i = 0; i < GamePanel.obs.num; i++) {
-            if (this.enemyRect.intersects(GamePanel.obs.getRect(i))) {
-            	if(enemyRect.x<= GamePanel.obs.getRect(i).x+31 && enemyRect.x>= GamePanel.obs.getRect(i).x && xDirection <=-1)
-            	if(xDirection != 0)
-            		setXDirection(0);
-            	if(yDirection != 0)
-            		setYDirection(0);
-            }
-        }
+        for (int i = 0; i < GamePanel.obs.num; i++)
+			if(GamePanel.obs.obs[i] != null)
+				if (GamePanel.obs.getRect(i).intersects(enemyRect)) {
+	            	if(enemyRect.x<= GamePanel.obs.getRect(i).x+31 && enemyRect.x>= GamePanel.obs.getRect(i).x && xDirection <=-1){
+            			this.xDirection = 0;
+            			//this.setCoord(enemyRect.x, enemyRect.y+1);
+	            	}
+	            	else if(enemyRect.x+32>= GamePanel.obs.getRect(i).x  && enemyRect.x+32<= GamePanel.obs.getRect(i).x+31 && xDirection >=1){
+            			this.xDirection = 0;
+	            		//this.setCoord(enemyRect.x, enemyRect.y+1);
+	            	}
+	            	else if(enemyRect.y+32>= GamePanel.obs.getRect(i).y && enemyRect.y+32<= GamePanel.obs.getRect(i).y+31 && yDirection >=1){
+            			this.yDirection = 0;
+            			//this.setCoord(enemyRect.x+1, enemyRect.y);
+	            	}
+	            	else if(enemyRect.y<= GamePanel.obs.getRect(i).y+321 && enemyRect.y>= GamePanel.obs.getRect(i).y && yDirection <=-1){
+            			this.yDirection = 0;
+	            		//this.setCoord(enemyRect.x+1, enemyRect.y);
+	            	}
+				}
        
     }
     
